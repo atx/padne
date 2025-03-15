3b478b344d143c6547b00356a583e40008737d2a
Josef Gajdusek <atx@atx.name>
2025-03-16 00:55:18 +0100

    feat: Add an implementation of the Mesh class

--- padne/mesh.py
+++ padne/mesh.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 from dataclasses import dataclass
-from typing import Optional, Iterable
+from typing import Optional, Iterable, Hashable
 
 # The purpose of this module is to generate triangular meshes from Shapely
 # (multi)polygons
@@ -51,7 +51,7 @@ class Point:
         return Vector(self.x - other.x, self.y - other.y)
 
 
-@dataclass
+@dataclass(eq=False)
 class Vertex:
     p: Point
     out: Optional["HalfEdge"] = None
@@ -65,7 +65,7 @@ class Vertex:
                 break
 
 
-@dataclass
+@dataclass(eq=False)
 class HalfEdge:
     origin: Vertex
     twin: Optional["HalfEdge"] = None
@@ -81,7 +81,7 @@ class HalfEdge:
         return self.twin.next.twin
 
 
-@dataclass
+@dataclass(eq=False)
 class Face:
     edge: HalfEdge = None
 
@@ -98,3 +98,103 @@ class Face:
     def vertices(self):
         for edge in self.edges:
             yield edge.origin
+
+
+class IndexMap[T: Hashable]:
+    """
+    A simple class that maps objects to indices and vice versa.
+    """
+
+    def __init__(self):
+        self._obj_to_idx: dict[T, int] = {}
+        self._idx_to_obj: list[T] = []
+
+    def add(self, obj: T) -> int:
+        # TODO: Maybe we do not want to allow adding the same object multiple
+        # times? This breaks the set .add interface, but we are realistically
+        # never going to do it and it could catch bugs
+        if obj not in self._obj_to_idx:
+            idx = len(self._idx_to_obj)
+            self._obj_to_idx[obj] = idx
+            self._idx_to_obj.append(obj)
+        return self._obj_to_idx[obj]
+
+    def to_index(self, obj: T) -> int:
+        return self._obj_to_idx[obj]
+
+    def to_object(self, idx: int) -> T:
+        return self._idx_to_obj[idx]
+
+    def __len__(self) -> int:
+        return len(self._idx_to_obj)
+
+    def items(self) -> Iterable[tuple[int, T]]:
+        for idx, obj in enumerate(self._idx_to_obj):
+            yield idx, obj
+
+
+class Mesh:
+    def __init__(self):
+        self.vertices = IndexMap[Vertex]()
+        self.halfedges = IndexMap[HalfEdge]()
+        self.faces = IndexMap[Face]()
+        self._edge_map: dict[tuple[int, int], HalfEdge] = {}
+
+    def make_vertex(self, p: Point) -> Vertex:
+        v = Vertex(p)
+        self.vertices.add(v)
+        return v
+
+    def connect_vertices(self, v1: Vertex, v2: Vertex) -> HalfEdge:
+        """
+        Return a half-edge between the two vertices v1 and v2. If the half
+        edge does not exist, create it. The twin half-edge is also created.
+        """
+        key12 = (self.vertices.to_index(v1), self.vertices.to_index(v2))
+        key21 = (key12[1], key12[0])
+        if key12 in self._edge_map:
+            return self._edge_map[key12]
+
+        # Assert that the twin also does not exist. It should not be possible
+        # to have one without the other
+        assert key21 not in self._edge_map, "Inconsistent half edge state"
+
+        e12 = HalfEdge(v1)
+        e21 = HalfEdge(v2)
+        e12.twin = e21
+        e21.twin = e12
+        self.halfedges.add(e12)
+        self.halfedges.add(e21)
+
+        self._edge_map[key12] = e12
+        self._edge_map[key21] = e21
+
+        # Update the vertex out pointers
+        if v1.out is None:
+            v1.out = e12
+        if v2.out is None:
+            v2.out = e21
+
+        return e12
+
+    def triangle_from_vertices(self, v1: Vertex, v2: Vertex, v3: Vertex) -> Face:
+        """
+        Create a triangle face from the three vertices.
+        """
+        e12 = self.connect_vertices(v1, v2)
+        e23 = self.connect_vertices(v2, v3)
+        e31 = self.connect_vertices(v3, v1)
+
+        f = Face(e12)
+        e12.next = e23
+        e23.next = e31
+        e31.next = e12
+
+        e12.face = e23.face = e31.face = f
+
+        self.faces.add(f)
+
+        return f
+
+    def euler_characteristic(self) -> int:
+        return len(self.vertices) - len(self.halfedges) // 2 + len(self.faces)
--- tests/test_mesh.py
+++ tests/test_mesh.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-from padne.mesh import Vector, Point, Vertex, HalfEdge, Face
+from padne.mesh import Vector, Point, Vertex, HalfEdge, Face, IndexMap, Mesh
 
 
 class TestVector:
@@ -257,3 +257,504 @@ class TestMeshStructure:
         assert orbit_edges[0] == e_out1
         assert orbit_edges[1] == e_out2
         assert orbit_edges[2] == e_out3
+
+    def test_vertex_hashability(self):
+        # Create vertices
+        v1 = Vertex(Point(1.0, 2.0))
+        v2 = Vertex(Point(1.0, 2.0))  # Same point, different object
+        v3 = Vertex(Point(3.0, 4.0))
+        
+        # Test hashability
+        hash(v1)  # This should not raise an exception
+        
+        # Test dictionary use
+        vertex_dict = {}
+        vertex_dict[v1] = "vertex 1"
+        vertex_dict[v3] = "vertex 3"
+        
+        # Verify dictionary behavior
+        assert len(vertex_dict) == 2
+        assert vertex_dict[v1] == "vertex 1"
+        assert vertex_dict[v3] == "vertex 3"
+        
+        # This depends on equality implementation - if vertices with same point are equal, this will pass
+        # Otherwise it would add a new entry
+        vertex_dict[v2] = "vertex 2"
+        assert len(vertex_dict) == 3  # Since v1 and v2 are different objects
+
+    def test_halfedge_hashability(self):
+        # Create vertices and half-edges
+        v1 = Vertex(Point(0.0, 0.0))
+        v2 = Vertex(Point(1.0, 0.0))
+        
+        e1 = HalfEdge(v1)
+        e2 = HalfEdge(v1)  # Same origin, different object
+        e3 = HalfEdge(v2)
+        
+        # Test hashability
+        hash(e1)  # This should not raise an exception
+        
+        # Test dictionary use
+        edge_dict = {}
+        edge_dict[e1] = "edge 1"
+        edge_dict[e3] = "edge 3"
+        
+        # Verify dictionary behavior
+        assert len(edge_dict) == 2
+        assert edge_dict[e1] == "edge 1"
+        assert edge_dict[e3] == "edge 3"
+        
+        # Add another edge with same origin
+        edge_dict[e2] = "edge 2"
+        assert len(edge_dict) == 3  # Since e1 and e2 are different objects
+
+    def test_face_hashability(self):
+        # Create faces
+        f1 = Face()
+        f2 = Face()
+        
+        # Test hashability
+        hash(f1)  # This should not raise an exception
+        
+        # Test dictionary use
+        face_dict = {}
+        face_dict[f1] = "face 1"
+        face_dict[f2] = "face 2"
+        
+        # Verify dictionary behavior
+        assert len(face_dict) == 2
+        assert face_dict[f1] == "face 1"
+        assert face_dict[f2] == "face 2"
+
+
+class TestMesh:
+    def test_initialization(self):
+        """Test that a new mesh is correctly initialized."""
+        mesh = Mesh()
+        assert len(mesh.vertices) == 0
+        assert len(mesh.halfedges) == 0
+        assert len(mesh.faces) == 0
+        assert len(mesh._edge_map) == 0
+
+    def test_make_vertex(self):
+        """Test vertex creation and registration."""
+        mesh = Mesh()
+        p = Point(1.0, 2.0)
+        
+        v = mesh.make_vertex(p)
+        
+        assert v.p == p
+        assert len(mesh.vertices) == 1
+        assert mesh.vertices.to_object(0) == v
+
+    def test_connect_vertices_new(self):
+        """Test connecting vertices with no existing connections."""
+        mesh = Mesh()
+        p1, p2 = Point(0.0, 0.0), Point(1.0, 0.0)
+        v1 = mesh.make_vertex(p1)
+        v2 = mesh.make_vertex(p2)
+        
+        e12 = mesh.connect_vertices(v1, v2)
+        
+        # Check properties of the edge
+        assert e12.origin == v1
+        assert e12.twin.origin == v2
+        assert e12.twin.twin == e12
+        
+        # Check registration
+        assert len(mesh.halfedges) == 2
+        assert e12 in [mesh.halfedges.to_object(i) for i in range(len(mesh.halfedges))]
+        assert e12.twin in [mesh.halfedges.to_object(i) for i in range(len(mesh.halfedges))]
+        
+        # Check edge map
+        v1_idx, v2_idx = mesh.vertices.to_index(v1), mesh.vertices.to_index(v2)
+        assert mesh._edge_map[(v1_idx, v2_idx)] == e12
+        assert mesh._edge_map[(v2_idx, v1_idx)] == e12.twin
+
+    def test_connect_vertices_existing(self):
+        """Test that connecting already connected vertices returns the existing edge."""
+        mesh = Mesh()
+        p1, p2 = Point(0.0, 0.0), Point(1.0, 0.0)
+        v1 = mesh.make_vertex(p1)
+        v2 = mesh.make_vertex(p2)
+        
+        e1 = mesh.connect_vertices(v1, v2)
+        e2 = mesh.connect_vertices(v1, v2)
+        
+        assert e1 == e2
+        assert len(mesh.halfedges) == 2  # Only one pair of half-edges created
+
+    def test_triangle_from_vertices(self):
+        """Test creating a triangle from three vertices."""
+        mesh = Mesh()
+        p1 = Point(0.0, 0.0)
+        p2 = Point(1.0, 0.0)
+        p3 = Point(0.0, 1.0)
+        
+        v1 = mesh.make_vertex(p1)
+        v2 = mesh.make_vertex(p2)
+        v3 = mesh.make_vertex(p3)
+        
+        face = mesh.triangle_from_vertices(v1, v2, v3)
+        
+        # Check that face was created properly
+        assert isinstance(face, Face)
+        assert face.edge.origin == v1
+        assert face.edge.next.origin == v2
+        assert face.edge.next.next.origin == v3
+        assert face.edge.next.next.next == face.edge  # Loop back to start
+        
+        # Check that edges point to this face
+        assert face.edge.face == face
+        assert face.edge.next.face == face
+        assert face.edge.next.next.face == face
+        
+        # Check that the face was registered
+        assert len(mesh.faces) == 1
+        assert mesh.faces.to_object(0) == face
+        
+        # Check half-edges
+        assert len(mesh.halfedges) == 6  # 3 edges * 2 half-edges each
+
+    def test_create_multiple_triangles(self):
+        """Test creating multiple connected triangles."""
+        mesh = Mesh()
+        
+        # Create a square with two triangles
+        p1 = Point(0.0, 0.0)
+        p2 = Point(1.0, 0.0)
+        p3 = Point(1.0, 1.0)
+        p4 = Point(0.0, 1.0)
+        
+        v1 = mesh.make_vertex(p1)
+        v2 = mesh.make_vertex(p2)
+        v3 = mesh.make_vertex(p3)
+        v4 = mesh.make_vertex(p4)
+        
+        f1 = mesh.triangle_from_vertices(v1, v2, v3)
+        f2 = mesh.triangle_from_vertices(v1, v3, v4)
+        
+        # Check registration
+        assert len(mesh.faces) == 2
+        assert len(mesh.vertices) == 4
+        
+        # Since some edges are shared, we expect fewer than 12 half-edges
+        # Each triangle adds 3 edges (6 half-edges), but they share 1 edge (2 half-edges)
+        assert len(mesh.halfedges) == 10
+        
+        # Check that edge v1->v3 is shared between the triangles
+        edge_v1v3 = None
+        for edge in f1.edges:
+            if edge.origin == v3 and edge.next.origin == v1:
+                edge_v1v3 = edge
+                break
+        
+        assert edge_v1v3 is not None
+        assert edge_v1v3.face == f1
+        assert edge_v1v3.twin.face == f2
+
+    def test_euler_characteristic(self):
+        """Test calculation of the Euler characteristic."""
+        mesh = Mesh()
+        
+        # Empty mesh
+        assert mesh.euler_characteristic() == 0
+        
+        # Single triangle (V=3, E=3, F=1 => 3-3+1 = 1)
+        p1 = Point(0.0, 0.0)
+        p2 = Point(1.0, 0.0)
+        p3 = Point(0.0, 1.0)
+        
+        v1 = mesh.make_vertex(p1)
+        v2 = mesh.make_vertex(p2)
+        v3 = mesh.make_vertex(p3)
+        
+        mesh.triangle_from_vertices(v1, v2, v3)
+        
+        assert mesh.euler_characteristic() == 1
+        
+        # Add a second triangle sharing an edge (V=4, E=5, F=2 => 4-5+2 = 1)
+        p4 = Point(1.0, 1.0)
+        v4 = mesh.make_vertex(p4)
+        mesh.triangle_from_vertices(v2, v4, v3)
+        
+        assert mesh.euler_characteristic() == 1
+
+    def test_edge_lookup(self):
+        """Test that edge lookup works correctly."""
+        mesh = Mesh()
+        
+        p1 = Point(0.0, 0.0)
+        p2 = Point(1.0, 0.0)
+        p3 = Point(0.0, 1.0)
+        
+        v1 = mesh.make_vertex(p1)
+        v2 = mesh.make_vertex(p2)
+        v3 = mesh.make_vertex(p3)
+        
+        # Create a triangle with direct edge connections first
+        e12 = mesh.connect_vertices(v1, v2)
+        e23 = mesh.connect_vertices(v2, v3)
+        e31 = mesh.connect_vertices(v3, v1)
+        
+        # Edge lookup should return the same edges
+        assert mesh.connect_vertices(v1, v2) == e12
+        assert mesh.connect_vertices(v2, v3) == e23
+        assert mesh.connect_vertices(v3, v1) == e31
+        
+        # Edge lookup should also work in reverse
+        assert mesh.connect_vertices(v2, v1).twin == e12
+        assert mesh.connect_vertices(v3, v2).twin == e23
+        assert mesh.connect_vertices(v1, v3).twin == e31
+
+    def test_connect_vertices_updates_out(self):
+        """Test that connect_vertices updates vertex out references."""
+        mesh = Mesh()
+        
+        p1 = Point(0.0, 0.0)
+        p2 = Point(1.0, 0.0)
+        
+        v1 = mesh.make_vertex(p1)
+        v2 = mesh.make_vertex(p2)
+        
+        # Initially vertices should have no out edge
+        assert v1.out is None
+        assert v2.out is None
+        
+        e12 = mesh.connect_vertices(v1, v2)
+        
+        # After connecting, vertices should have out edges
+        # Note: This depends on the implementation details of connect_vertices
+        # so we might need to adjust this test if the implementation changes
+        assert v1.out is not None
+        assert v2.out is not None
+
+    def test_triangle_from_vertices_reuses_edges(self):
+        """Test that triangle_from_vertices reuses existing edges."""
+        mesh = Mesh()
+        
+        p1 = Point(0.0, 0.0)
+        p2 = Point(1.0, 0.0)
+        p3 = Point(0.0, 1.0)
+        
+        v1 = mesh.make_vertex(p1)
+        v2 = mesh.make_vertex(p2)
+        v3 = mesh.make_vertex(p3)
+        
+        # Create edge first
+        e12 = mesh.connect_vertices(v1, v2)
+        
+        # Create triangle that should reuse this edge
+        face = mesh.triangle_from_vertices(v1, v2, v3)
+        
+        edge_count = len(mesh.halfedges) // 2  # Count actual edges, not half-edges
+        
+        # Should have exactly 3 edges (not 4) since one was reused
+        assert edge_count == 3
+        
+        # The triangle's first edge should be the reused edge
+        assert face.edge == e12
+
+    def test_duplicate_triangles(self):
+        """Test that creating identical triangles doesn't duplicate data."""
+        mesh = Mesh()
+        
+        p1 = Point(0.0, 0.0)
+        p2 = Point(1.0, 0.0)
+        p3 = Point(0.0, 1.0)
+        
+        v1 = mesh.make_vertex(p1)
+        v2 = mesh.make_vertex(p2)
+        v3 = mesh.make_vertex(p3)
+        
+        f1 = mesh.triangle_from_vertices(v1, v2, v3)
+        f2 = mesh.triangle_from_vertices(v1, v2, v3)
+        
+        # The faces are new objects but should reuse the same edges
+        assert f1 != f2
+        assert len(mesh.faces) == 2
+        assert len(mesh.halfedges) == 6  # Still only 3 edges (6 half-edges)
+
+    def test_non_manifold_edge_handling(self):
+        """Test behavior when creating non-manifold edges."""
+        mesh = Mesh()
+        
+        # Create a triangle
+        p1 = Point(0.0, 0.0)
+        p2 = Point(1.0, 0.0)
+        p3 = Point(0.0, 1.0)
+        p4 = Point(0.0, -1.0)
+        
+        v1 = mesh.make_vertex(p1)
+        v2 = mesh.make_vertex(p2)
+        v3 = mesh.make_vertex(p3)
+        v4 = mesh.make_vertex(p4)
+        
+        # Create first triangle
+        f1 = mesh.triangle_from_vertices(v1, v2, v3)
+        
+        # Create second triangle sharing an edge but in same direction
+        # This creates a non-manifold edge where one edge is used by two faces
+        # Implementation details will determine the exact behavior here
+        f2 = mesh.triangle_from_vertices(v1, v2, v4)
+        
+        # Both faces should exist
+        assert len(mesh.faces) == 2
+        
+        # The edge from v1 to v2 should be used by both faces
+        # How this is handled depends on implementation, so we can't make
+        # many assertions about structure integrity here
+
+    def test_complex_mesh(self):
+        """Test creating a more complex mesh structure."""
+        mesh = Mesh()
+        
+        # Create a cube-like structure (without top face)
+        # Bottom face
+        p1 = Point(0.0, 0.0)
+        p2 = Point(1.0, 0.0)
+        p3 = Point(1.0, 1.0)
+        p4 = Point(0.0, 1.0)
+        
+        # Top vertices
+        p5 = Point(0.0 + 10, 0.0)
+        p6 = Point(1.0 + 10, 0.0)
+        p7 = Point(1.0 + 10, 1.0)
+        p8 = Point(0.0 + 10, 1.0)
+        
+        # Create vertices
+        vertices = []
+        for p in [p1, p2, p3, p4, p5, p6, p7, p8]:
+            vertices.append(mesh.make_vertex(Point(p.x, p.y)))  # Using only x,y since our mesh is 2D
+        
+        # Create base face (square with diagonal)
+        f1 = mesh.triangle_from_vertices(vertices[0], vertices[1], vertices[2])
+        f2 = mesh.triangle_from_vertices(vertices[0], vertices[2], vertices[3])
+        
+        # Create side faces
+        f3 = mesh.triangle_from_vertices(vertices[0], vertices[1], vertices[4])
+        f4 = mesh.triangle_from_vertices(vertices[1], vertices[5], vertices[4])
+        
+        f5 = mesh.triangle_from_vertices(vertices[1], vertices[2], vertices[5])
+        f6 = mesh.triangle_from_vertices(vertices[2], vertices[6], vertices[5])
+        
+        f7 = mesh.triangle_from_vertices(vertices[2], vertices[3], vertices[6])
+        f8 = mesh.triangle_from_vertices(vertices[3], vertices[7], vertices[6])
+        
+        f9 = mesh.triangle_from_vertices(vertices[3], vertices[0], vertices[7])
+        f10 = mesh.triangle_from_vertices(vertices[0], vertices[4], vertices[7])
+        
+        # Check face count
+        assert len(mesh.faces) == 10
+        
+        # Edge count should be less than what we'd need for separate triangles
+        # due to shared edges
+        assert len(mesh.halfedges) / 2 < 3 * 10  # Less than 30 edges for 10 triangles
+        
+        # Verify Euler characteristic for this mesh
+        # V = 8, E varies based on shared edges, F = 10
+        # For a topological cube with one face removed: V - E + F = 2 - 1 = 1
+        # (may vary depending on the exact triangle configuration)
+        assert mesh.euler_characteristic() == 1
+
+
+class TestIndexMap:
+    def test_initialization(self):
+        # Test that an empty IndexMap has length 0
+        index_map = IndexMap()
+        assert len(index_map) == 0
+
+    def test_add_single_object(self):
+        # Test adding a single object
+        index_map = IndexMap()
+        idx = index_map.add("test")
+        assert idx == 0
+        assert len(index_map) == 1
+
+    def test_add_multiple_objects(self):
+        # Test adding multiple different objects
+        index_map = IndexMap()
+        idx1 = index_map.add("one")
+        idx2 = index_map.add("two")
+        idx3 = index_map.add("three")
+        
+        assert idx1 == 0
+        assert idx2 == 1
+        assert idx3 == 2
+        assert len(index_map) == 3
+
+    def test_add_duplicate_objects(self):
+        # Test that adding the same object twice returns the same index
+        index_map = IndexMap()
+        idx1 = index_map.add("repeated")
+        idx2 = index_map.add("repeated")
+        
+        assert idx1 == idx2
+        assert len(index_map) == 1
+
+    def test_to_index(self):
+        # Test retrieving index from object
+        index_map = IndexMap()
+        index_map.add("apple")
+        index_map.add("banana")
+        
+        assert index_map.to_index("apple") == 0
+        assert index_map.to_index("banana") == 1
+        
+        with pytest.raises(KeyError):
+            # Non-existent object should raise KeyError
+            index_map.to_index("orange")
+
+    def test_to_object(self):
+        # Test retrieving object from index
+        index_map = IndexMap()
+        index_map.add("apple")
+        index_map.add("banana")
+        
+        assert index_map.to_object(0) == "apple"
+        assert index_map.to_object(1) == "banana"
+        
+        with pytest.raises(IndexError):
+            # Invalid index should raise IndexError
+            index_map.to_object(2)
+
+    def test_items(self):
+        # Test the items iterator
+        index_map = IndexMap()
+        objects = ["one", "two", "three"]
+        for obj in objects:
+            index_map.add(obj)
+        
+        items = list(index_map.items())
+        assert len(items) == 3
+        
+        for i, (idx, obj) in enumerate(items):
+            assert idx == i
+            assert obj == objects[i]
+
+    def test_different_object_types(self):
+        # Test with various object types
+        index_map = IndexMap()
+        
+        # String
+        str_idx = index_map.add("string")
+        assert str_idx == 0
+        
+        # Integer
+        int_idx = index_map.add(42)
+        assert int_idx == 1
+        
+        # Tuple (hashable)
+        tuple_idx = index_map.add((1, 2, 3))
+        assert tuple_idx == 2
+        
+        # Custom object (Point)
+        point = Point(1.0, 2.0)
+        point_idx = index_map.add(point)
+        assert point_idx == 3
+        
+        # Verify all objects
+        assert index_map.to_object(str_idx) == "string"
+        assert index_map.to_object(int_idx) == 42
+        assert index_map.to_object(tuple_idx) == (1, 2, 3)
+        assert index_map.to_object(point_idx) == point
