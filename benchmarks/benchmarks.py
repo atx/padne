312d1549e38bdac2355aa79d36a9ca3346a2cddf
Josef Gajdusek <atx@atx.name>
2025-09-27 14:39:37 +0200

    bench: Add elementary benchmark suite

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,6 +1,12 @@
 
+import os
+import pathlib
+import sys
 import shapely.geometry
 from padne.mesh import Mesher
+from padne import kicad, solver
+
+from tests.conftest import _kicad_test_projects
 
 
 class MesherSuite:
@@ -56,3 +62,60 @@ class MesherSuite:
         ['default', 'relaxed', 'fixed_density']  # config names
     )
     time_mesh_generation.param_names = ['geometry', 'config']
+
+    def track_triangle_count(self, geometry_name, config_name):
+        """Track the number of triangles generated for different geometries and configurations."""
+        geometry = self.geometries[geometry_name]
+        config = self.configs[config_name]
+
+        mesher = Mesher(config)
+        mesh = mesher.poly_to_mesh(geometry)
+        return len(mesh.faces)
+
+    # Define parameters for the benchmark
+    track_triangle_count.params = (
+        ['normal_rect', 'large_rect', 'rect_with_hole'],  # geometry names
+        ['default', 'relaxed', 'fixed_density']  # config names
+    )
+    track_triangle_count.param_names = ['geometry', 'config']
+
+
+class KicadSuite:
+    """Benchmarks for KiCad project loading with different test projects."""
+
+    def setup(self, *_):
+        """Discover and set up test KiCad projects using the existing test infrastructure."""
+        # Get all available test projects
+        self.test_projects = _kicad_test_projects()
+
+    def time_kicad_project_loading(self, project_name):
+        """Time KiCad project loading for different test projects."""
+        project = self.test_projects[project_name]
+        # Use the project's pro_path to call load_kicad_project
+        kicad.load_kicad_project(project.pro_path)
+
+    # Define parameters for the benchmark
+    time_kicad_project_loading.params = ['two_big_planes', 'via_tht_4layer']
+    time_kicad_project_loading.param_names = ['project']
+
+
+class SolverSuite:
+
+    def setup_cache(self, *_):
+        test_projects = _kicad_test_projects()
+        project_names = self.time_solver_solve.params
+        loaded_cache = {}
+        for project_name in project_names:
+            project = test_projects[project_name]
+            loaded_cache[project_name] = kicad.load_kicad_project(project.pro_path)
+
+        # Hopefully this will get properly pickled...
+        return loaded_cache
+
+    def time_solver_solve(self, loaded_projects, project_name):
+        project = loaded_projects[project_name]
+        # Just solve the project, we don't care about the result here
+        solver.solve(project)
+
+    time_solver_solve.params = ['two_big_planes', 'via_tht_4layer']
+    time_solver_solve.param_names = ['project']
